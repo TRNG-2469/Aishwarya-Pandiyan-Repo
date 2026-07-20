d49c2c48cb28c63d54f3f958458fd59cd9a6d76c
Aishwarya Pandiyan <apandiya@charlotte.edu>
2026-07-20 16:56:39 -0400

    Complete arrays-practice exercise

---
+++ Week1-Java/ArrayAnalyzer.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+
+public class ArrayAnalyzer {
+    public static void main(String[] args) {
+        int[] values = new int[10];
+        Random rand = new Random();
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = rand.nextInt(100) + 1;
+        }
+
+        int sum = 0;
+        int min = values[0];
+        int max = values[0];
+
+        for (int i = 0; i < values.length; i++) {
+            sum += values[i];
+
+            if (values[i] < min) {
+                min = values[i];
+            }
+
+            if (values[i] > max) {
+                max = values[i];
+            }
+        }
+
+        double average = (double) sum / values.length;
+
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < values.length; i++) {
+            sb.append(values[i]);
+            if (i < values.length - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+
+        System.out.println("Array: " + sb.toString());
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+    }
+}
